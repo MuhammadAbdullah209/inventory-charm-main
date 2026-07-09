8cf27ea4b601d547b89e76a2efe810f3a37cfb61
M.Abdullah <abdullah488838@gmail.com>
2026-07-10 00:44:34 +0500

    fix-mongodb-timout-problem

--- Backend/db.js
+++ Backend/db.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const Dbconnector = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+    });
     console.log("Db Connection Stable!");
   } catch (error) {
     console.log("Db Connection No longer live", error);
   }
 };
+
+export default Dbconnector;
 
 export default Dbconnector;
