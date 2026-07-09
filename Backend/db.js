2b440b64df798f2309e96a970100c59505802bcf
M.Abdullah <abdullah488838@gmail.com>
2026-07-10 00:48:04 +0500

    revert-timeout

--- Backend/db.js
+++ Backend/db.js
@@ -2,16 +2,11 @@ import mongoose from "mongoose";
 
 const Dbconnector = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Db Connection Stable!");
   } catch (error) {
     console.log("Db Connection No longer live", error);
   }
 };
-
-export default Dbconnector;
 
 export default Dbconnector;
