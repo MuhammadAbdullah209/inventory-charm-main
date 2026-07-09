2600e28d6b35787f7019453591a3dd8ec66cd2cc
M.Abdullah <abdullah488838@gmail.com>
2026-07-10 00:10:19 +0500

    fix-option-wild-card

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
-app.options("*", cors());
+app.options("(.*)", cors());
 await Dbconnector();
 
 // Handle preflight requests
